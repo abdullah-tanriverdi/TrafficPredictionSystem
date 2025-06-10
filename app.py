3f90873172541f5372b783d720553e8393a1cb3b
abdullah-tanriverdi <4abdullah.tanriverdi@gmail.com>
2025-06-10 03:20:26 +0300

    Complementary logical structure and core processing framework have been implemented.

--- app.py
+++ app.py
@@ -12,20 +12,18 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     m = folium.Map(location=[41.0603, 28.9870], zoom_start=14, tiles=None)
-
     folium.TileLayer(
-    tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-    attr='Esri',
-    name='Esri World Imagery',
-    overlay=False,
-    control=False
-).add_to(m)
-    
+        tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr='Esri',
+        name='Esri World Imagery',
+        overlay=False,
+        control=False
+    ).add_to(m)
+
     return render_template('index.html', map=m._repr_html_(), start='', end='', error=None,
                            start_coords=None, end_coords=None)
 
-@app.route('/generate_route', methods=['POST'])
-
+@app.route('/render_route', methods=['POST'])
 def generate_route():
     start_input = request.form['start']
     end_input = request.form['end']
@@ -44,56 +42,34 @@ def generate_route():
     m = folium.Map(location=[center_lat, center_lon], zoom_start=14)
 
     folium.TileLayer(
-    tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-    attr='Esri',
-    name='Esri World Imagery',
-    overlay=False,
-    control=False
-).add_to(m)
+        tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr='Esri',
+        name='Esri World Imagery',
+        overlay=False,
+        control=False
+    ).add_to(m)
 
-    folium.Marker(
-        [start_lat, start_lon],
-        popup="Başlangıç",
-         icon=folium.Icon(color='green', icon='play', prefix='fa')
-         ).add_to(m)
-    folium.Marker(
-        [end_lat, end_lon],
-        popup="Bitiş",
-        icon=folium.Icon(color='red', icon='stop', prefix='fa')
-        ).add_to(m)
+    folium.Marker([start_lat, start_lon], popup="Başlangıç",
+                  icon=folium.Icon(color='green', icon='play', prefix='fa')).add_to(m)
+    folium.Marker([end_lat, end_lon], popup="Bitiş",
+                  icon=folium.Icon(color='red', icon='stop', prefix='fa')).add_to(m)
 
-    padding = 0.02  
-    lats = [start_lat, end_lat]
-    lons = [start_lon, end_lon]
-    lat_min = min(lats) - padding
-    lat_max = max(lats) + padding
-    lon_min = min(lons) - padding
-    lon_max = max(lons) + padding
+    
+    initial_padding = 0.03
+    initial_lat_min = min(start_lat, end_lat) - initial_padding
+    initial_lat_max = max(start_lat, end_lat) + initial_padding
+    initial_lon_min = min(start_lon, end_lon) - initial_padding
+    initial_lon_max = max(start_lon, end_lon) + initial_padding
 
-    traffic_data = fetch_traffic_data(lat_min, lat_max, lon_min, lon_max)
-    if traffic_data is None:
+    initial_traffic_data = fetch_traffic_data(initial_lat_min, initial_lat_max, initial_lon_min, initial_lon_max)
+    if initial_traffic_data is None:
         error = "Trafik verisi alınırken hata oluştu."
         return render_template('index.html', map=m._repr_html_(), start=start_input, end=end_input,
                                start_coords=(round(start_lat,6), round(start_lon,6)),
                                end_coords=(round(end_lat,6), round(end_lon,6)),
                                error=error)
 
-    heatmap_data = []
-    for segment in traffic_data:
-        location = segment.get("location", {})
-        shape = location.get("shape", {})
-        links = shape.get("links", [])
-        current_flow = segment.get("currentFlow", {})
-        jam_factor = current_flow.get("jamFactor", 1.0)
-        for link in links:
-            points = link.get("points", [])
-            for pt in points:
-                if lat_min <= pt['lat'] <= lat_max and lon_min <= pt['lng'] <= lon_max:
-                    heatmap_data.append([pt['lat'], pt['lng'], jam_factor])
-    if heatmap_data:
-        HeatMap(heatmap_data, radius=25, blur=20, max_zoom=14).add_to(m)
-
-    G = build_weighted_graph(traffic_data, lat_min, lat_max, lon_min, lon_max)
+    G = build_weighted_graph(initial_traffic_data, initial_lat_min, initial_lat_max, initial_lon_min, initial_lon_max)
     start_node = find_nearest_node(G, (start_lat, start_lon))
     end_node = find_nearest_node(G, (end_lat, end_lon))
 
@@ -112,28 +88,70 @@ def generate_route():
                                end_coords=(round(end_lat,6), round(end_lon,6)),
                                error=error)
 
-    folium.PolyLine(route_coords, color="blue", weight=5, opacity=0.7).add_to(m)
-    turn_by_turn = get_turn_by_turn(route_coords)
+    folium.PolyLine(route_coords, color="#FFFFFF", weight=8, opacity=1, tooltip="AKILLI ROTA", sticky=True).add_to(m)
 
-    total_length_m = sum(geodesic(route_coords[i], route_coords[i+1]).meters for i in range(len(route_coords)-1))
+    
+    route_lats = [coord[0] for coord in route_coords]
+    route_lons = [coord[1] for coord in route_coords]
+    padding = 0.005
+
+    lat_min = min(route_lats) - padding
+    lat_max = max(route_lats) + padding
+    lon_min = min(route_lons) - padding
+    lon_max = max(route_lons) + padding
+
+    folium.Rectangle(bounds=[[lat_min, lon_min], [lat_max, lon_max]],
+                     color='red', weight=3, fill=False).add_to(m)
+
+   
+    traffic_data = fetch_traffic_data(lat_min, lat_max, lon_min, lon_max)
+
+  
+    heatmap_data = []
+    road_info = []
+
+    if traffic_data:
+        for segment in traffic_data:
+            location = segment.get("location", {})
+            description = location.get("description", "Bilinmeyen Yol")
+            current_flow = segment.get("currentFlow", {})
+            jam_factor = current_flow.get("jamFactor", 0)
+            speed = current_flow.get("speed", 0)
+
+            road_info.append({
+                "name": description,
+                "jam_factor": jam_factor,
+                "speed": round(speed, 2)
+            })
+
+            shape = location.get("shape", {})
+            links = shape.get("links", [])
+            for link in links:
+                for pt in link.get("points", []):
+                    lat = pt['lat']
+                    lon = pt['lng']
+                    if lat_min <= lat <= lat_max and lon_min <= lon <= lon_max:
+                        heatmap_data.append([lat, lon, jam_factor])
+
+    if heatmap_data:
+        HeatMap(heatmap_data, radius=25, blur=20, max_zoom=14).add_to(m)
+
+    turn_by_turn = get_turn_by_turn(route_coords)
+    total_length_m = sum(geodesic(route_coords[i], route_coords[i + 1]).meters for i in range(len(route_coords) - 1))
     average_speed_kmh = 30
     total_time_min = round((total_length_m / 1000) / average_speed_kmh * 60, 1)
 
     osrm_route, osrm_directions, osrm_total_length_km, osrm_total_time_min, osrm_average_speed = get_osrm_route((start_lat, start_lon), (end_lat, end_lon))
+    street_names = []
+
     if osrm_route:
-        folium.PolyLine(osrm_route, color="#FFFFFF", weight=8, opacity=1, tooltip="OSRM Rotası", sticky=True).add_to(m)
+        folium.PolyLine(osrm_route, color="#800080", weight=8, opacity=1, tooltip="OSRM ROTA", sticky=True).add_to(m)
         street_names = extract_street_names(osrm_directions)
-    else:
-        osrm_directions = None
-        osrm_total_length_km = None
-        osrm_total_time_min = None
-        osrm_average_speed = None
-        street_names = []
 
     return render_template('index.html', map=m._repr_html_(), start=start_input, end=end_input,
-                           start_coords=(round(start_lat,6), round(start_lon,6)),
-                           end_coords=(round(end_lat,6), round(end_lon,6)),
-                           total_length_km=round(total_length_m / 1000, 2),
+                           start_coords=(round(start_lat, 6), round(start_lon, 6)),
+                           end_coords=(round(end_lat, 6), round(end_lon, 6)),
+                           total_length_km=round(total_length_m),
                            total_time_min=total_time_min,
                            turn_by_turn=turn_by_turn,
                            osrm_directions=osrm_directions,
@@ -141,8 +159,8 @@ def generate_route():
                            osrm_total_time_min=osrm_total_time_min,
                            osrm_average_speed=osrm_average_speed,
                            street_names=street_names,
+                           road_info=road_info,
                            error=None)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
--- run.py
+++
@@ -1,7 +0,0 @@
-from geopy.distance import geodesic
-
-start = (40.991295, 29.024563)
-end = (41.025636, 29.096305)
-
-distance_m = geodesic(start, end).meters
-print(f"Mesafe: {distance_m:.2f} metre")
